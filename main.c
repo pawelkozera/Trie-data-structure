c7386d68696083973c102a425416f26d5f97b69b
p0js <ztraktor2@gmail.com>
2022-03-19 14:48:58 +0100

    Dodano wstawianie i wyszukiwanie

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define LITERY_ALFABETU 32
+#define LITERY_ALFABETU 26
 
 struct Trie {
     int jest_lisciem; // 1 gdy jest lisciem, 0 gdy nie jest
     struct Trie* litery[LITERY_ALFABETU];
 };
 
-
+//Porotypy funkcji
 struct Trie* stworz_nowe_drzewo_trie();
+void wstaw_do_drzewa(struct Trie *korzen, char slowo[]);
+int wyszukaj_z_drzewa(struct Trie *korzen, char *slowo);
+void zamien_na_male_litery(char slowo[]);
+
 
 int main()
 {
     struct Trie* korzen = stworz_nowe_drzewo_trie();
 
+    char z[] = "Kot";
+    wstaw_do_drzewa(korzen, z);
+    printf("%d\n", wyszukaj_z_drzewa(korzen, "kot"));
+
     return 0;
 }
 
+//Funkcje
 struct Trie* stworz_nowe_drzewo_trie() {
     struct Trie* drzewo = (struct Trie*)malloc(sizeof(struct Trie));
     drzewo->jest_lisciem = 0;
@@ -28,3 +38,49 @@ struct Trie* stworz_nowe_drzewo_trie() {
 
     return drzewo;
 };
+
+/*
+'a' == 97
+'b' == 98
+'a' - 'a' = 0
+'b' - 'a' = 1
+odejmujac 'a' otrzymujemy indeksy od 0 do 25, dla alfabetu od a do z bez Polskich znakow
+*/
+void wstaw_do_drzewa(struct Trie *korzen, char slowo[]) {
+    struct Trie* aktualny_wezel = korzen;
+    zamien_na_male_litery(slowo);
+
+    while (*slowo) {
+        if (aktualny_wezel->litery[*slowo - 'a'] == NULL) {
+            aktualny_wezel->litery[*slowo - 'a'] = stworz_nowe_drzewo_trie();
+        }
+        aktualny_wezel = aktualny_wezel->litery[*slowo - 'a'];
+        slowo++;
+    }
+
+    aktualny_wezel->jest_lisciem = 1;
+}
+
+void zamien_na_male_litery(char slowo[]) {
+    for (int i = 0; i < strlen(slowo); i++) {
+        slowo[i] = tolower(slowo[i]);
+    }
+}
+
+// zwraca 1 jestli slowo jest w drzewie, 0 jesli go nie ma
+int wyszukaj_z_drzewa(struct Trie *korzen, char *slowo) {
+    if (korzen == NULL) {
+        return 0; // drzewo jest puste
+    }
+
+    struct Trie* aktualny_wezel = korzen;
+    while (*slowo) {
+        aktualny_wezel = aktualny_wezel->litery[*slowo - 'a'];
+        if (aktualny_wezel == NULL) {
+            return 0;
+        }
+        slowo++;
+    }
+
+    return aktualny_wezel->jest_lisciem;
+}
