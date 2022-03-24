851fc3a55448fd4ac8e9f01a117b0164c3689963
p0js <ztraktor2@gmail.com>
2022-03-24 15:42:20 +0100

    Dodanie usuwania z drzewa

--- main.c
+++ main.c
@@ -15,14 +15,17 @@ struct Trie* stworz_nowe_drzewo_trie();
 void wstaw_do_drzewa(struct Trie *korzen, char slowo[]);
 int wyszukaj_z_drzewa(struct Trie *korzen, char *slowo);
 void zamien_na_male_litery(char slowo[]);
-
+int usun_z_drzewa(struct Trie **aktualny_wezel, char *slowo);
+int ma_pod_wezly(struct Trie *aktualny_wezel);
 
 int main()
 {
     struct Trie* korzen = stworz_nowe_drzewo_trie();
 
-    char z[] = "Kot";
+    char z[] = "kot";
     wstaw_do_drzewa(korzen, z);
+    printf("%d\n", wyszukaj_z_drzewa(korzen, "kot"));
+    usun_z_drzewa(&korzen, "kot");
     printf("%d\n", wyszukaj_z_drzewa(korzen, "kot"));
 
     return 0;
@@ -84,4 +87,47 @@ int wyszukaj_z_drzewa(struct Trie *korzen, char *slowo) {
     }
 
     return aktualny_wezel->jest_lisciem;
+}
+
+int ma_pod_wezly(struct Trie *aktualny_wezel) {
+    for (int i = 0; i < LITERY_ALFABETU; i++) {
+        if (aktualny_wezel->litery[i]) {
+            return 1; // znaleziono
+        }
+    }
+
+    return 0;
+}
+
+int usun_z_drzewa(struct Trie **aktualny_wezel, char *slowo) {
+    if (*aktualny_wezel == NULL) {
+        return 0; // jeœli drzewo jest puste zwracamy 0
+    }
+
+    if (*slowo) {
+        if ((*aktualny_wezel)->litery[*slowo - 'a'] != NULL && usun_z_drzewa(&((*aktualny_wezel)->litery[*slowo - 'a']), slowo + 1) && (*aktualny_wezel)->jest_lisciem == 0) {
+            if (!ma_pod_wezly(*aktualny_wezel)) {
+                free(*aktualny_wezel);
+                (*aktualny_wezel) == NULL;
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
+    if (*slowo == '\0' && (*aktualny_wezel)->jest_lisciem) {
+        if (!ma_pod_wezly(*aktualny_wezel)) {
+            free(*aktualny_wezel);
+            (*aktualny_wezel) = NULL;
+            return 1;
+        }
+        else {
+            (*aktualny_wezel)->jest_lisciem = 0;
+            return 0;
+        }
+    }
+
+    return 0;
 }
